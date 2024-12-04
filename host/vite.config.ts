6fb56751bd0a1c2acadc3fa7aeaec0041c616470
ArkadyCH <arkadychu4vaga@gmail.com>
2024-12-04 09:50:53 +0300

    feat(mfe): fix missing styles from remote routes expose

--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import federation from "@originjs/vite-plugin-federation";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,9 +12,20 @@ export default defineConfig({
       remotes: {
         starships: "http://localhost:5173/assets/remoteEntry.js",
       },
-      shared: ["vue", "@apollo/client"],
+      shared: [
+        "vue",
+        "@apollo/client",
+        "@vue/apollo-composable",
+        "graphql",
+        "graphql-tag",
+      ],
     }),
   ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   build: {
     minify: false,
     target: ["chrome89", "edge89", "firefox89", "safari15"],
